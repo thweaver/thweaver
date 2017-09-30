05bc60adef687916e6ef462a791717cdb794f5ff
The Firm Graphics <thefirmgraphics@Thes-iMac.local>
2017-09-29 19:08:12 -0600

    Add home work area

--- tw/index.php
+++ tw/index.php
@@ -1,26 +1,37 @@
 <?php include 'header.php' ?>
 	<div class="hero">
 		<div class="hero-container">
-			<div class="hero-copy">
+			<!-- <div class="hero-copy">
 				<h1><span>Thomas</span> Weaver</h1>
-				<h2>Multidisciplinary* Designer based in  Denver, CO
-	</h2>
-	<h3>* &nbsp;
-		<span class="home-brag"> UX/UI Design</span>
-		<span class="home-brag"> Front End Web Development</span>
-		<span class="home-brag"> Responsive Web Design</span>
-		<span class="home-brag"> Graphic Design & Branding</span>
-		<span class="home-brag"> Product Design</span>
-		<span class="home-brag"> Art Direction</span>
-		<span class="home-brag"> Digital Illustration</span>
-		<span class="home-brag"> Brand Development</span>
-		
-	</h3>
-			</div>
+				<h2>Multidisciplinary* Designer based in  Denver, CO</h2>
+				<h3>* &nbsp;
+					<span class="home-brag"> UX/UI Design</span>
+					<span class="home-brag"> Front End Web Development</span>
+					<span class="home-brag"> Responsive Web Design</span>
+					<span class="home-brag"> Graphic Design & Branding</span>
+					<span class="home-brag"> Product Design</span>
+					<span class="home-brag"> Art Direction</span>
+					<span class="home-brag"> Digital Illustration</span>
+					<span class="home-brag"> Brand Development</span>
+				</h3>
+			</div> -->
 		</div>
 		<div class="scroll-prompt">
 			<?php include 'img/scroll-prompt.svg' ?>
 		</div>
 	</div>
-	<div class="home-work"></div>
+	<div class="home-work">
+		<h2>Recent Work</h2>
+		<div class="home-work-grid">
+			<a  href="" class="home-project">
+				<img src="img/temp-project-thumb.jpg">
+			</a>
+			<a  href="" class="home-project">
+				<img src="img/temp-project-thumb.jpg">
+			</a>
+			<a  href="" class="home-project">
+				<img src="img/temp-project-thumb.jpg">
+			</a>
+		</div>
+	</div>
 <?php include 'footer.php' ?>
